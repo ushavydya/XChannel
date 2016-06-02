db1ad1a58b90209266b6b80fcf3508ad444b1f98
Usha Vydyanathan <ushavydya@Ushas-MacBook-Pro.local>
2016-06-02 13:24:14 -0700

    Adding XChannel ios SDK

---
+++ XChannel.framework/Headers/XChannel-umbrella.h
@@ -0,0 +1,22 @@
+#import <UIKit/UIKit.h>
+
+#import "Configuration.h"
+#import "XCBeacon.h"
+#import "XCBeaconDeal.h"
+#import "XCCampaign.h"
+#import "XCCampaignSocial.h"
+#import "XChannel.h"
+#import "XCUser.h"
+#import "NSData+Base64.h"
+#import "XCBeaconRegion.h"
+#import "XCCommonUtils.h"
+#import "XCConfiguration.h"
+#import "XChannelGlobals.h"
+#import "XChannelSDK.h"
+#import "XChannel_iOS_SDK.h"
+#import "XCLocationManager.h"
+#import "XCServerProxy.h"
+
+FOUNDATION_EXPORT double XChannelVersionNumber;
+FOUNDATION_EXPORT const unsigned char XChannelVersionString[];
+
